52fc649606e030622e1384ddd76e52c775d4ad5d
Goodness Johnson <goodnessogedengbe54@gmail.com>
2025-01-30 07:13:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SUBZERO-MD~3zoxxQKQ#WgFgIWow4i3Rrsuzcdw0UZubx_RNx3_zqrjdxjQ-s5k || "",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
